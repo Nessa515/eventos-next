95947cdda39f5166b293c1321948014b5d5525d9
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-11-06 21:51:53 -0400

    Feitos pagers e layout

---
+++ src/app/eventos/[id]/page.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function EventosId({ params}){
+  return(
+    <>
+      Evento com o Id: {params.id}
+    </>
+  )
+}
---
+++ src/app/eventos/page.jsx
@@ -0,0 +1,10 @@
+import Cabecalho from "@/components/Cabecalho";
+
+export default function Eventos(){
+    return(
+        <div>
+          <Cabecalho />
+          <h2>Lista de eventos</h2>
+        </div>
+    )
+}
